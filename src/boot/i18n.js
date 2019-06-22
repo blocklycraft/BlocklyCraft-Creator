ced342641aded817533a801adba77c92c10a77ca
hempflower <1993996310@qq.com>
2019-06-22 22:09:37 +0800

    I18n!

--- src/boot/i18n.js
+++ src/boot/i18n.js
@@ -1,12 +1,26 @@
 import VueI18n from 'vue-i18n'
 import messages from 'src/i18n'
+import Quasar from 'quasar'
 
-export default async ({ app, Vue }) => {
+function getCookie(name)
+{
+  let arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
+
+  if(arr=document.cookie.match(reg))
+
+    return unescape(arr[2]);
+  else
+    return null;
+}
+
+export default async ({ app, Vue}) => {
   Vue.use(VueI18n);
+  let u_local = '';
+  u_local = getCookie("language") || Quasar.lang.getLocale();
 
   // Set i18n instance on app
   app.i18n = new VueI18n({
-    locale: 'zh-cn',
+    locale: u_local,
     fallbackLocale: 'zh-cn',
     messages
   })
