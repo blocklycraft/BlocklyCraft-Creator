968f41bb6106053f51cc48e1c35600533d67bbb6
hempflower <1993996310@qq.com>
2019-06-28 17:35:16 +0800

    尝试加入黑暗模式

--- src/boot/init.js
+++ src/boot/init.js
@@ -1,13 +1,29 @@
 // import something here
 import Common from '../components/Common'
 import Snotify from 'vue-snotify'
-import "vue-snotify/styles/material.css";
+import "vue-snotify/styles/material.css"
+const settings = require('electron-settings');
+import theme_light from '../theme/light'
+import theme_dark from '../theme/dark'
+
+
 
 // "async" is optional
-export default ({ Vue, app /* app, router, Vue, ... */ }) => {
+export default ({ Vue, /* app, router, Vue, ... */ }) => {
   // something to do
   Vue.use(Snotify);
 
   Vue.prototype.BlockCraft = Common;
-  Vue.prototype.eventHub= new Vue()
+  Vue.prototype.eventHub= new Vue();
+  //register themes
+
+  //Read theme config from file
+  Common.dark = settings.get('dark');
+  if(!Common.dark){
+    //Load light theme
+    Common.applyTheme(theme_light);
+  }else{
+    Common.applyTheme(theme_dark);
+  }
+
 }
