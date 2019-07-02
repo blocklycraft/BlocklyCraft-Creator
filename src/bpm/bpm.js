63da0349bea22726446d58456536652212e02683
hempflower <1993996310@qq.com>
2019-07-02 18:54:22 +0800

    Hello bpm!

---
+++ src/bpm/bpm.js
@@ -0,0 +1,29 @@
+let path = '';
+let version = '0.0.1';
+
+export default {
+    getVersion (){
+        return version;
+    },
+    setDirectory (path){
+        
+    },
+    getDirectory (){
+        return path;
+    },
+    refreshLib (){
+
+    },
+    getLib (){
+
+    },
+    installLibOffline (path){
+
+    },
+    installLibOnline (name, repos){
+
+    },
+    uninstallLib (name){
+
+    }
+}
