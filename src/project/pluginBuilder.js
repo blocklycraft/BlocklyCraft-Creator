9349bfc2c7d2df182e8d4dcdc9f7055a89196a5d
hempflower <1993996310@qq.com>
2019-08-04 23:58:59 +0800

    完成部分插件构建

--- src/project/pluginBuilder.js
+++ src/project/pluginBuilder.js
@@ -1,3 +1,8 @@
+const Downloader = require("filedownloader");
+const {app} = require('electron').remote;
+let fs = require("fs");
+let unzip = require("unzip");
+
 export default {
     buildPlugin(path){
 
@@ -6,10 +11,41 @@ export default {
 
     },
     genJar(path){
+        //主要工作:mvn package
+        let shell = require('shelljs');
+        if(shell.which('mvn')){
+            //用户没有安装maven,下载一个maven.....
+
+        }
 
     },
     genPluginYml(path){
 
+    },
+    checkMaven(){
+
+    },
+    checkJava(){
+
+    },
+    downloadMaven(callback){
+        setTimeout(function (){
+            const Dl = new Downloader({
+                url: "https://mirrors.tuna.tsinghua.edu.cn/apache/maven/maven-3/3.6.1/binaries/apache-maven-3.6.1-bin.zip",
+                saveTo:app.getPath("userData"),
+                saveAs:"apache-maven-3.6.1-bin.zip",
+                resume:true
+            }).on("progress", progress => callback({status:"process",proecess:progress}))
+            .on("end", ()=> callback({status:"finish"}))
+            .on("error", ()=> callback({status:"error"}))
+            .on("start", ()=> callback({status:"start"}))
+
+        },0)
+    },
+    unZipMaven(callback){
+        setTimeout(function (){
+            //明天继续....
+        },0)
     }
     
 }
